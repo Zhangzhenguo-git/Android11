cdba912cc87402f6fcaa1bc8ad990173d8b534dd
张振国 <826189539@qq.com>
2020-07-07 14:46:39 +0800

    1111

--- app/src/main/java/com/zshg/android11/MainActivity.java
+++ app/src/main/java/com/zshg/android11/MainActivity.java
@@ -1,17 +1,32 @@
 package com.zshg.android11;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CoordinatorLayout coordinatorLayout;
+    private FloatingActionButton snackbar_fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Snackbar.make(mLayout,
-                R.string.camera_permission_available,
-                Snackbar.LENGTH_SHORT).show();
+        coordinatorLayout=findViewById(R.id.coordinator);
+        snackbar_fab=findViewById(R.id.snackbar_fab);
+        snackbar_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(coordinatorLayout,
+                        R.string.camera_permission_available,
+                        Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
